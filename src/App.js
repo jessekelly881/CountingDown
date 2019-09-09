e619da2ac40d88b61e18327fb792d8d5ecb9db4e
Jesse Kelly <kellyjesse881@gmail.com>
2019-09-09 11:44:20 +0000

    form created with verification

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
 import React, {useState} from "react";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-import { DatePicker, TimePicker, Button, Input} from 'antd';
+import { DatePicker, TimePicker, Button, Input, Form} from 'antd';
 const { TextArea } = Input;
 
 import 'antd/dist/antd.css';
 
 import "./App.scss"
 
-function CreateCounterForm() {
+
+function CreateCounterForm(props) {
+
+    function handleCreateCounterForm(e) {
+        e.preventDefault();
+
+        props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+    const { getFieldDecorator } = props.form;
 
     return (
-        <>
-        <Input placeholder="Countdown Name" />
-        <TextArea rows={4} placeholder="Description" />
-        <DatePicker /> <TimePicker /> <br />
+      <Form onSubmit={handleCreateCounterForm} className="create-counter-form">
 
-        <Button block>Create Timer</Button> <br />
-        </>
+        <Form.Item>
+        {
+            getFieldDecorator('name', {
+            rules: [{ required: true, message: 'Please enter a name for your counter' }],
+            })
+
+            ( <Input placeholder="Countdown Name" /> )
+        }
+
+        </Form.Item>
+
+        <Form.Item>
+        {
+            getFieldDecorator('description', {
+            rules: [{ required: true, message: 'Please enter a description for your counter' }],
+            })
+
+            (<TextArea rows={4} placeholder="Description" />)
+        }
+
+        </Form.Item>
+
+        <Form.Item>
+        {
+            getFieldDecorator('date_time', {
+            rules: [{ required: true, message: 'Please enter a date and time' }],
+            })
+
+            (<DatePicker showTime format="YYYY-MM-DD HH:mm:ss" placeholder="Select date and time" />)
+        }
+        </Form.Item>
+
+        <Form.Item>
+        <Button block htmlType="submit">Create Timer</Button> <br />
+        </Form.Item>
+
+      </Form>
     )
 }
+
+const WrappedCreateCounterForm = Form.create({ name: 'create_counter' })(CreateCounterForm);
 
 function IndexPage() {
     return (
       <>
-        <CreateCounterForm />
+        <div className="create-counter">
+          <WrappedCreateCounterForm />
+        </div>
       </>
     )
 }
